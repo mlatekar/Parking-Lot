8e7a802499537ac7b25efe614e9f50b7bb017e8c
mlatekar <mlatekar@gmail.com>
2020-03-20 05:26:32 +0530

    ADDED:Welcome Message

---
+++ src/main/java/ParkingLot/ParkingLotAnalyser.java
@@ -0,0 +1,12 @@
+package ParkingLot;
+
+public class ParkingLotAnalyser {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Parking-Lot");
+    }
+}
+
+
---
+++ src/test/java/ParkingLotTest.java
@@ -0,0 +1,17 @@
+import ParkingLot.ParkingLotAnalyser;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ParkingLotTest {
+    ParkingLotAnalyser parkingLotAnalyser = null;
+
+    @Before
+    public void beforeClass() throws Exception {
+
+        parkingLotAnalyser = new ParkingLotAnalyser();
+    }
+
+
+}
